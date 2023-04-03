cabf6f3928b665d5fcf148c1b113e4e0e4eb5873
Regiana Barbosa <regiana.cruz@acad.ifma.edu.br>
2023-04-03 23:02:27 +0100

    fixed productRepository id

--- src/main/java/com/isep/acme/domain/repository/ProductRepository.java
+++ src/main/java/com/isep/acme/domain/repository/ProductRepository.java
@@ -36,7 +36,7 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
   @Query("UPDATE Product p SET p.sku = :sku WHERE p.sku=:sku")
   Product updateBySku(@Param("sku") String sku);
 
-  @Query("SELECT p FROM Product p WHERE p.productID=:productID")
-  Optional<Product> findById(Long productID);
+  @Query("SELECT p FROM Product p WHERE p.productId=:productId")
+  Optional<Product> findById(Long productId);
 
 }
